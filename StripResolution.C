87dc3e7bed28c0fbce4d0542d5ea5a1c7b9681dc
Philipp Bielefeldt <p.bielefeldt@hiskp.uni-bonn.de>
2019-03-19 15:19:21 +0100

    calculate cog

--- StripResolution.C
+++ StripResolution.C
@@ -1,3 +1,20 @@
+Double_t get_centre(TH1I* h)
+{
+  double amp_sum=0;
+  double x_a_sum=0;
+  
+  for (Int_t bin=1; bin<=h->GetNbinsX(); bin++) // beware the ROOT numbering
+  {
+    Int_t    a = h->GetBinContent(bin); 
+    Double_t c = h->GetXaxis()->GetBinCenter(bin); 
+    
+    amp_sum += a;
+    x_a_sum += a*c;
+  }
+  
+  return x_a_sum/amp_sum;
+}
+
 void StripResolution()
 {
   const Double_t x_min = -40;
@@ -13,10 +30,13 @@ void StripResolution()
 
   // the impeding signal (don't change  it)
   TF1 *mc_signal = new TF1("MCSignal", "gaus(0)", x_min, x_max); 
-  mc_signal->SetParameters(4, 0, 1);
+  Double_t mc_mu = 0; //TODO: vary about one bin width
+  mc_signal->SetParameters(4, mc_mu, 1);
 
-  // "strip plane"
+  // fill "strip plane"
   TH1I *plane = new TH1I("p", "strip plane", granularity, x_min, x_max);
   plane->FillRandom("MCSignal", amp_n);
   
+  // reconstruction of the measusrement
+  const Double_t cog =  get_centre(plane);
 }
